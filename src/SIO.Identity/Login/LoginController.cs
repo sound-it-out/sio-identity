82051815199a17af57fe903c7a9da4cc250fc1f1
Matt Hope <matthew.hope396@outlook.com>
2019-10-22 13:59:28 +0100

    WIP: login flow

--- src/SIO.Identity/Login/LoginController.cs
+++ src/SIO.Identity/Login/LoginController.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
 using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SIO.Identity.Login.Requests;
+using SIO.Identity.Login.Responses;
 using SIO.Migrations;
 
 namespace SIO.Identity.Login
@@ -16,20 +20,53 @@ namespace SIO.Identity.Login
         private readonly IIdentityServerInteractionService _interaction;
         private readonly UserManager<SIOUser> _userManager;
         private readonly SignInManager<SIOUser> _signInManager;
+        private readonly IConfiguration _configuration;
+        private readonly IAuthenticationSchemeProvider _schemeProvider;
+        private readonly IClientStore _clientStore;
+
+        public LoginController(IIdentityServerInteractionService interaction,
+            UserManager<SIOUser> userManager, 
+            SignInManager<SIOUser> signInManager, 
+            IConfiguration configuration,
+            IAuthenticationSchemeProvider schemeProvider,
+            IClientStore clientStore)
+        {
+            if (interaction == null)
+                throw new ArgumentNullException(nameof(interaction));
+            if (userManager == null)
+                throw new ArgumentNullException(nameof(userManager));
+            if (signInManager == null)
+                throw new ArgumentNullException(nameof(signInManager));
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+            if (schemeProvider == null)
+                throw new ArgumentNullException(nameof(schemeProvider));
+            if (clientStore == null)
+                throw new ArgumentNullException(nameof(clientStore));
+
+            _interaction = interaction;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+            _schemeProvider = schemeProvider;
+            _clientStore = clientStore;
+        }
+
 
         [HttpGet("login")]
         public async Task<IActionResult> Login(string returnUrl)
         {
             if (User.Identity.IsAuthenticated)
-                if (_interaction.IsValidReturnUrl(returnUrl))
+                if (_interaction.IsValidReturnUrl(returnUrl) || Url.IsLocalUrl(returnUrl))
                     return Redirect(returnUrl);
                 else
-                    return Redirect("");
+                    return Redirect(_configuration.GetValue<string>("DefaultAppUrl"));
 
-            return View();
+            return View(await BuildResponseAsync(returnUrl));
         }
 
         [HttpPost("login")]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequest request, string button)
         {
             var context = await _interaction.GetAuthorizationContextAsync(request.ReturnUrl);
@@ -37,11 +74,14 @@ namespace SIO.Identity.Login
             if (button != "login")
             {
                 if (context == null)
-                    return Redirect("");
+                    return Redirect(_configuration.GetValue<string>("DefaultAppUrl"));
 
                 await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
 
-                return Redirect(request.ReturnUrl);
+                if (_interaction.IsValidReturnUrl(request.ReturnUrl) || Url.IsLocalUrl(request.ReturnUrl))
+#pragma warning disable SCS0027 // Open redirect: possibly unvalidated input in {1} argument passed to '{0}'
+                    return Redirect(request.ReturnUrl);
+#pragma warning restore SCS0027 // Open redirect: possibly unvalidated input in {1} argument passed to '{0}'
             }
 
             if (ModelState.IsValid)
@@ -50,7 +90,25 @@ namespace SIO.Identity.Login
 
                 if (user == null)
                 {
-                    // throw error
+                    ModelState.AddModelError("", "Please ensure that your username and password are correct.");
+
+                    return View(await BuildResponseAsync(null));
+                }
+
+                if (await _userManager.IsLockedOutAsync(user))
+                {
+                    ModelState.AddModelError("", "Your account has been locked please try again in a few minutes");
+
+                    return View(await BuildResponseAsync(null));
+                }
+
+                if (!user.EmailConfirmed)
+                {
+                    ModelState.AddModelError("", "Your account is not verified, but a new activation link has been sent to your email.");
+
+                    await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    return View(await BuildResponseAsync(null));
                 }
 
                 var validLogin = await _userManager.CheckPasswordAsync(user, request.Password);
@@ -58,18 +116,52 @@ namespace SIO.Identity.Login
 
                 if (!validLogin || user.IsArchived)
                 {
-                    // throw error
+                    await _userManager.AccessFailedAsync(user);
+                    ModelState.AddModelError("", "Please ensure that your username and password are correct.");
+
+                    return View(await BuildResponseAsync(null));
                 }
 
                 await _signInManager.SignInAsync(user, false);
+                await _userManager.ResetAccessFailedCountAsync(user);
 
                 if (_interaction.IsValidReturnUrl(request.ReturnUrl) || Url.IsLocalUrl(request.ReturnUrl))
+#pragma warning disable SCS0027 // Open redirect: possibly unvalidated input in {1} argument passed to '{0}'
                     return Redirect(request.ReturnUrl);
-                 
-                return Redirect("");
+#pragma warning restore SCS0027 // Open redirect: possibly unvalidated input in {1} argument passed to '{0}'
+
+                return Redirect(_configuration.GetValue<string>("DefaultAppUrl"));
             }
 
-            return Redirect("");
+            return Redirect(_configuration.GetValue<string>("DefaultAppUrl"));
+        }
+
+        private async Task<LoginResponse> BuildResponseAsync(string returnUrl)
+        {
+            var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
+
+            if (context?.IdP != null)
+                throw new NotImplementedException();
+
+            var allowLocal = true;
+            var schemes = await _schemeProvider.GetAllSchemesAsync();
+            var providers = schemes.Where(scheme => !string.IsNullOrWhiteSpace(scheme.DisplayName))
+                                   .Select(scheme => new ExternalProvider(scheme.DisplayName, scheme.Name));
+
+            if (context?.ClientId != null)
+            {
+                var client = await _clientStore.FindEnabledClientByIdAsync(context.ClientId);
+
+                if (client != null)
+                {
+                    allowLocal = client.EnableLocalLogin;
+
+                    if (client.IdentityProviderRestrictions != null && client.IdentityProviderRestrictions.Any())
+                        providers = providers.Where(provider => client.IdentityProviderRestrictions.Contains(provider.Scheme));
+                }
+            }
+
+            return new LoginResponse(context?.LoginHint, returnUrl, allowLocal, providers);
         }
     }
 }
