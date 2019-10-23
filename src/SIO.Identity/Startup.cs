f206bba04ed4134f73f96b99975d6e0a46261d7f
Matt Hope <matthew.hope396@outlook.com>
2019-10-23 07:28:37 +0100

    Fixed issue with razor views.

--- src/SIO.Identity/Startup.cs
+++ src/SIO.Identity/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OpenEventSourcing.Extensions;
@@ -29,6 +30,11 @@ namespace SIO.Identity
                 .AddJsonSerializers();
             
             services.AddSIOIdentity();
+
+            services.Configure<RazorViewEngineOptions>(o =>
+            {
+                o.ViewLocationFormats.Add($"/{{1}}/Views/{{0}}{RazorViewEngine.ViewExtension}");
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
