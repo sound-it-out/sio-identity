ca351e16a5aadf7e107588694a35fedc0c48e73e
Matt Hope <matthew.hope396@outlook.com>
2020-11-14 15:04:07 +0000

    Fixed issues with identity server not having data base up to date.

--- src/SIO.Migrations/Migrations/IdentityServer/PersistedGrantDb/20201114150155_UpdateIdentityServer.Designer.cs
+++ src/SIO.Migrations/Migrations/IdentityServer/PersistedGrantDb/20201114150155_UpdateIdentityServer.Designer.cs
@@ -4,14 +4,16 @@ using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SIO.Migrations.Migrations.IdentityServer.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    partial class PersistedGrantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201114150155_UpdateIdentityServer")]
+    partial class UpdateIdentityServer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
