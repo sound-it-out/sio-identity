d40c0b89341d9bd3f5422cec150305584611934f
Matt Hope <matthew.hope396@outlook.com>
2020-11-21 16:36:33 +0000

    Added error page.

---
+++ src/SIO.Identity/Error/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SIO.Identity.Error
+{
+    public class ErrorController : Controller
+    {
+        private readonly IIdentityServerInteractionService _identityServerInteractionService;
+
+        public ErrorController(IIdentityServerInteractionService identityServerInteractionService)
+        {
+            if (identityServerInteractionService == null)
+                throw new ArgumentNullException(nameof(identityServerInteractionService));
+
+            _identityServerInteractionService = identityServerInteractionService;
+        }
+
+        [HttpGet("error")]
+        public async Task<IActionResult> Error(string errorId)
+        {
+            var errorContext = await _identityServerInteractionService.GetErrorContextAsync(errorId);
+            return View(errorContext);
+        }
+    }
+}
